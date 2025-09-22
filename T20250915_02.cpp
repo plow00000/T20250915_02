88dd48f360ae994a64ec370d0df961f8baf6a542
plow00000 <icusene@gmail.com>
2025-09-22 11:10:02 +0900

    Update T20250915_02.cpp

--- T20250915_02.cpp
+++ T20250915_02.cpp
@@ -6,13 +6,36 @@ int main()
 {
     int Number[10] = { 0, };
 
-    for (int i = 0; i < 5; ++i)
+    int Count = 0;
+
+    cin >> Count;
+
+    cout << endl;
+
+    for (int i = 0; i < Count; ++i)
     {
         for (int j = 0; j < i; ++j)
         {
             cout << ' ';
         }
-        for (int j = i; j < 5; ++j)
+
+        for (int j = i; j < Count; ++j)
+        {
+            cout << '*';
+        }
+
+        cout << endl;
+    }
+
+    cout << endl << endl;
+
+    for (int i = 0; i < Count; ++i)
+    {
+        for (int j = 0; j < i; ++j)
+        {
+            cout << ' ';
+        }
+        for (int j = i; j < Count * 2 - i; ++j)
         {
             cout << '*';
         }
