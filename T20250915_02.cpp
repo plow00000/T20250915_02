9ce92445b90af360aecad4a34601154d8ec1f695
plow00000 <icusene@gmail.com>
2025-09-22 16:43:32 +0900

    플레이어 이동 및 벽 판정

--- T20250915_02.cpp
+++ T20250915_02.cpp
@@ -1,46 +1,106 @@
 #include <iostream>
+#include <conio.h>
 
 using namespace std;
 
+//타일 배치 정보. 현재 0은 빈칸, 1은 벽
+int World[10][10] = {
+	{ 01, 01, 01, 01, 01, 01, 01, 01, 01, 01 },
+	{ 01, 00, 00, 00, 00, 00, 00 ,00, 00, 01 },
+	{ 01, 00, 00, 00, 00, 00, 00 ,00, 00, 01 },
+	{ 01, 00, 00, 00, 00, 00, 00 ,00, 00, 01 },
+	{ 01, 00, 00, 00, 00, 00, 00 ,00, 00, 01 },
+	{ 01, 00, 00, 00, 00, 00, 00 ,00, 00, 01 },
+	{ 01, 00, 00, 00, 00, 00, 00 ,00, 00, 01 },
+	{ 01, 00, 00, 00, 00, 00, 00 ,00, 00, 01 },
+	{ 01, 00, 00, 00, 00, 00, 00 ,00, 00, 01 },
+	{ 01, 01, 01, 01, 01, 01, 01, 01, 01, 01 }
+};
+char Tileset[10] = { ' ', '*', }; //어떤 모양이 그려질지에 대한 정보
+
+bool bIsRunning = true;
+
+int PlayerX = 1;
+int PlayerY = 1;
+char PlayerShape = 'P';
+
+int KeyCode = 0;
+
+void Input()
+{
+	KeyCode = _getch();
+}
+
+void Process()
+{
+	switch (KeyCode)
+	{
+	case 'w':
+		if (World[--PlayerY][PlayerX] != 0)
+		{
+			++PlayerY;
+		}
+		break;
+	case 's':
+		if (World[++PlayerY][PlayerX] != 0)
+		{
+			--PlayerY;
+		}
+		break;
+	case 'a':
+		if (World[PlayerY][--PlayerX] != 0)
+		{
+			++PlayerX;
+		}
+		break;
+	case 'd':
+		if (World[PlayerY][++PlayerX] != 0)
+		{
+			--PlayerX;
+		}
+		break;
+	case 'q':
+		bIsRunning = false;
+		break;
+	default:
+		break;
+	}
+}
+
+void Render()
+{
+	system("cls");
+	for (int Y = 0; Y < 10; ++Y)
+	{
+		for (int X = 0; X < 10; ++X)
+		{
+			if (PlayerX == X && PlayerY == Y)
+			{
+				cout << PlayerShape;
+			}
+			else
+			{
+				cout << Tileset[World[Y][X]];
+			}
+			cout << ' ';
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
 int main()
 {
-    int Number[10] = { 0, };
 
-    int Count = 0;
+	//frame, DeltaSecond
+	while (bIsRunning)
+	{
+		Input();
+		
+		Process();
 
-    cin >> Count;
+		Render();
+	}
 
-    cout << endl;
-
-    for (int i = 0; i < Count; ++i)
-    {
-        for (int j = 0; j < i; ++j)
-        {
-            cout << ' ';
-        }
-
-        for (int j = i; j < Count; ++j)
-        {
-            cout << '*';
-        }
-
-        cout << endl;
-    }
-
-    cout << endl << endl;
-
-    for (int i = 0; i < Count; ++i)
-    {
-        for (int j = 0; j < i; ++j)
-        {
-            cout << ' ';
-        }
-        for (int j = i; j < Count * 2 - i; ++j)
-        {
-            cout << '*';
-        }
-        cout << endl;
-    }
-
-    return 0;
+	return 0;
 }
