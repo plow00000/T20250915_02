bce10eab7e2e93f092685b8fc699118aa8b114bc
plow00000 <icusene@gmail.com>
2025-09-22 09:23:19 +0900

    Update T20250915_02.cpp

--- T20250915_02.cpp
+++ T20250915_02.cpp
@@ -4,18 +4,18 @@ using namespace std;
 
 int main()
 {
-    int sum = 0;
+    int Sum = 0;
     int MultiplOf3 = 0;
 
     for (int i = 1; i <= 100; i++)
     {
-        sum += i;
+        Sum += i;
         if (i % 3 == 0)
         {
             MultiplOf3 += i;
 		}
     }
 
-    cout << "1~100까지의 모든 숫자의 합 : " << sum << endl;
+    cout << "1~100까지의 모든 숫자의 합 : " << Sum << endl;
 	cout << "1~100까지의  3의 배수의 합 : " << MultiplOf3 << endl;
 }
