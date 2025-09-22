c356d51cb1b09aae8d8235283c8822dff692024a
plow00000 <icusene@gmail.com>
2025-09-22 09:09:46 +0900

    20250922과제_수정

--- T20250915_02.cpp
+++ T20250915_02.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
     int sum = 0;
-    for (int i = 1; i <= 100; i++) {
+
+    for (int i = 1; i <= 100; i++)
+    {
         sum += i;
     }
-    std::cout << "1~100까지의 합 : " << sum << std::endl;
+
+    cout << "1~100까지의 합 : " << sum << endl;
 }
